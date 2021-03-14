6d471551f1bbdb1db35f6c06e64437da91278b8c
Khury <huskystar33@gmail.com>
2021-03-15 08:54:08 +0900

    오류 수정

--- src/main.cpp
+++ src/main.cpp
@@ -38,6 +38,7 @@ int main(int argc, const char** argv) {
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT,GL_TRUE);
 
     // glfw 윈도우 생성, 실패하면 에러 출력후 종료
     SPDLOG_INFO("Create glfw window");
